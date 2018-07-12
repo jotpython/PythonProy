952cf23fc4a7d9819403abf5c0d8c4db6c1dee74
jotpython <41133460+jotpython@users.noreply.github.com>
2018-07-11 22:21:01 -0700

    upd1

--- myRed.py
+++ myRed.py
@@ -11,3 +11,13 @@ print("""
 |/     \|   \_/   |/   \__/(_______/(______/                                                             
 """)
 
+print("Solo para conocernos un poco mejor")
+print("Entre los datos que necesitamos a continuacion")
+nombre = input("Escriba su nombre")
+fn = input("Anno de Nacimiento")
+
+def cal_edad(a):
+    edad = 2018 - int(a)
+    print(edad)
+
+cal_edad(fn)
