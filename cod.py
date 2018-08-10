c2e4b3e8bf5ab936b5b198aa9a77cc4f7ec778cf
Jorgemb <jmendez200488t@gmail.com>
2018-08-09 21:51:05 -0800

    Merge branch 'master' of https://github.com/jotpython/PythonProy
    # Conflicts:
    #	cod.py

--- cod.py
+++ cod.py
@@ -88,8 +88,16 @@ i = 0
 while i<len(s):
   resultado= resultado + s[len(s)-i-1]
   i=i+1
-print(resultado)"""
+print(resultado)
 
 s = "Acaso hubo buhos aca"
 t = s[2:9]+s[0:1]
-print(t)
+print(t)"""
+
+ejemplo = open("probando.txt", "r")
+a = ejemplo.readline()
+b = ejemplo.readline()
+c = ejemplo.readline()
+d = a + c
+print(d)
+
